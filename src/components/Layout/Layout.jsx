19d7c05a68c5652810c5a579ec0018d28f6c67e0
Elucidator0409 <tranquockhanhdhsp99@gmail.com>
2023-06-09 16:50:21 +0700

    first commit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+
+
+import React from 'react'
+
+import Header from 'components/Header/Header'
+import Footer from 'components/Footer/Footer'
+import Routers from 'routers/Routers'
+
+const Layout = () => {
+  return (
+    <>
+        <Header />
+        <div>
+            <Routers />
+        </div>
+        <Footer />
+    </>
+  )
+}
+
+export default Layout
